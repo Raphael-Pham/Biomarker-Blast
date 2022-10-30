0b5082e29e437130552c0954fd44347d14354653
Raphael Pham <raphaelpham14@gmail.com>
2022-10-29 21:34:54 -0700

    Completed gene_id to fasta file conversion using GenBank HTML & Entrez API call

--- constants.py
+++ constants.py
@@ -2,3 +2,6 @@
 MARKERDB_API = "http://markerdb.ca/api/v1/"
 MARKERDB_CONDITIONS = "https://markerdb.ca/conditions/"
 
+# GenBank
+GENBANK_BASE = "https://www.ncbi.nlm.nih.gov/"
+GENBANK_GENE = "https://www.ncbi.nlm.nih.gov/gene/?term="
--- markerdb_webscraping.py
+++ markerdb_webscraping.py
@@ -1,9 +1,11 @@
+import os
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 import lxml
 import cchardet
 import re
 import time
+from Bio import Entrez, SeqIO
 
 from markerdb_api import markerdb_get
 import constants
@@ -15,8 +17,10 @@ def build_soup(url, requests_session, type):
         return BeautifulSoup(page.text, "lxml")
     elif type == "biomarker":
         return BeautifulSoup(page.text, "lxml", parse_only=SoupStrainer("div", {"id": "Sequence_Variants"}))
-    elif type == "sequence_variant":
-            return BeautifulSoup(page.text, "lxml", parse_only=SoupStrainer("a"))
+    elif type == "sequence_variant" or type == "genbank":
+        return BeautifulSoup(page.text, parse_only=SoupStrainer("a"), features="xml")
+    elif type == "fasta":
+        return BeautifulSoup(page.text, features="xml")
 
 def get_biomarker_ids(condition_id, requests_session):
     gene_ids = set()
@@ -49,16 +53,37 @@ def get_biomarker_ids(condition_id, requests_session):
     print()
 
     return gene_ids
-    
+
+def save_gene_sequences(biomarker_ids, requests_session):
+    print()
+    print("Saving fasta files...")
+    for biomarker_id in biomarker_ids:
+        soup = build_soup(constants.GENBANK_GENE + biomarker_id, requests_session, "genbank")
+        ncbi_string = r'(.*)ncbi_uid=' + re.escape(str(biomarker_id)) + r'(.*)report=fasta'
+        
+        file_extension = soup.find_all('a', ref=re.compile(ncbi_string), limit=1)[0]['href']
+        accession_num = file_extension[9:].split('?')[0]
+        from_to = file_extension.split('&')
+        from_idx = from_to[0].split('=')[-1]
+        to_idx = from_to[1].split('=')[-1]
+
+        handle = Entrez.efetch(db='nuccore', id=accession_num, seq_start=from_idx, seq_stop=to_idx, rettype='fasta', retmode='text')
+        fasta_filename = "/genbank_fasta/" + accession_num + '_' + from_idx + '_' + "to" + '_' + to_idx + ".fasta"
+        SeqIO.write(SeqIO.read(handle, 'fasta'), os.path.dirname(os.path.realpath(__file__)) + fasta_filename, 'fasta')
+        time.sleep(1)                                   # Prevent NCBI from closing connection due to frequency of efetch requests
+        print(fasta_filename + " saved!")
+
 def scrape_marker_db():
     requests_session = requests.Session()
-    user_input = "Homocystinuria"
+    user_input = "Usher Syndrome Type I"
+    Entrez.email = "raphaelpham14@gmail.com"
 
     condition_html = markerdb_get("condition", QUERY=user_input, PAGE=1)['conditions'][0]
     print("\nRetrieved " + condition_html['name'] + " data...")
     print("Fetching all associated biomarker GenBank IDs...")
     biomarker_ids = get_biomarker_ids(str(condition_html['id']), requests_session)
     print("Biomarker IDs: " + str(biomarker_ids))
+    save_gene_sequences(biomarker_ids, requests_session)
 
 if __name__ == "__main__":
     scrape_marker_db()
